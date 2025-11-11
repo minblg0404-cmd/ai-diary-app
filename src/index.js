20287e353146cd0320be1e8d4c3c17429bf30d45
Miyuki Maruyama <min.blg.0404@gmail.com>
2025-11-11 21:52:37 +0900

    index.js修正

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// import "@aws-amplify/ui-react/styles.css";  // ←
+import './index.css'; // ← これがないと Tailwind が効かない
 import { Amplify } from "aws-amplify";
 import awsExports from "./aws-exports";
 
